89a8809613ac9f708810c7284f59b233b6e6482c
tanyatiwari <getrjtanyatiwari@gmail.com>
2025-05-27 08:42:02 +0530

    first commitsing

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -6,7 +6,7 @@ const razorpay = new Razorpay({
   key_id:process.env.RAZORPAY_KEY_ID,
   key_secret:process.env.RAZORPAY_SECRET,
 
-});
+});;
 
 export const createOrder = async (req, res) => {
   try {
